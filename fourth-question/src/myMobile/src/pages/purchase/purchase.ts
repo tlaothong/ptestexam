8ad6a257cf0933e2855115259b730e29af7e9f73
Earn <earndarkness@perfenterprise.com>
2018-09-19 17:07:21 +0700

    implement mobile for purchase product implement port for webapi

--- fourth-question/src/myMobile/src/pages/purchase/purchase.ts
+++ fourth-question/src/myMobile/src/pages/purchase/purchase.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @IonicPage()
 @Component({
@@ -9,7 +10,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class PurchasePage {
 
   search: string;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  quantity: string;
+
+  model: any = [];
+  totalprice: number = 0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private httpClient: HttpClient) {
   }
-  
+
+  searchproduct() {
+    this.httpClient.get("https://localhost:5001/api/Values/CalculateTotalPrice/" + this.search + "/" + this.quantity)
+      .subscribe((data: any) => {
+        if (data.productDetail == null) alert("Product is not found");
+        else {
+          this.model.push(data);
+          this.totalprice += data.totalPrice;
+        }
+      }, error => {
+        console.log("Purchase Error: " + JSON.stringify(error));
+      });
+  }
 }
