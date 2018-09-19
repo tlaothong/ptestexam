ca6c903d028082e4e44c367a103e5422a8ca408e
kritsada <jokerstudio@hotmail.com>
2018-09-19 18:58:52 +0700

    add bmi api

---
+++ src/question1/Calculator.Api/Controllers/CalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<CalculateResult> Get()
+        {
+            var bmiCalculator = new BMICalculator();
+            var result = bmiCalculator.Calculate(50, 40);
+            return result;
+        }
+    }
+}
