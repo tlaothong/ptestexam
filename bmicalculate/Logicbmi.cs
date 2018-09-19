f88666aed46f5b193b53f4cb3922eb86e4f9679d
Aphaphon <pui.aphaphon@gmail.com>
2018-09-19 13:36:15 +0700

    update test

--- bmicalculate/Logicbmi.cs
+++ bmicalculate/Logicbmi.cs
@@ -5,15 +5,7 @@ namespace bmicalculate
         public double BMI(double weight  , double hight)
         {
             double bmi = weight/(hight*hight)*703;
-            if(bmi >=18.5)
-            {
-                return bmi;
-                
-            }
-            else
-            {
-                return bmi;
-            }  
+           return bmi;  
         }
     }
 }
