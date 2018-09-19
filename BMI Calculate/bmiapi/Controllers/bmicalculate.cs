6775ab7818299191df59df6639013eeb823da01f
powerofwind <powerofwind10@gmail.com>
2018-09-19 15:25:53 +0700

    create api(complete),test(reference complete) folder ionic

---
+++ BMI Calculate/bmiapi/Controllers/bmicalculate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bmiapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class bnicalculate : ControllerBase
+    {
+        [HttpGet]
+        public double Get(double weight, double height)
+        {
+            var a = new calculatelogic();
+            var result = a.bmi(weight,height);
+              return result;
+        }
+    }
+}
---
+++ BMI Calculate/bmiapi/culculatelogic.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace bmiapi
+{
+    public class calculatelogic
+    {
+        public double bmi(double weight, double height)
+        {
+            return 0;
+        }
+        
+    }
+}
