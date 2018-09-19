eb2e7652dab21213dec5ff6f48019ecbdf0f9cb6
Aphaphon <pui.aphaphon@gmail.com>
2018-09-19 11:04:38 +0700

    setup and create model
    Signed-off-by: Aphaphon <pui.aphaphon@gmail.com>

---
+++ CatalogPos/Controllers/ProductController.1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogPos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {       
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
---
+++ CatalogPos/Models/Products.cs
@@ -0,0 +1,9 @@
+namespace CatalogPos.Models
+{
+    public class Products
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string price { get; set; }
+    }
+}
