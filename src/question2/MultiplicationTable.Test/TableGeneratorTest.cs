eac892d2e121a277758c46176953046b1ee9a4d7
kritsada <jokerstudio@hotmail.com>
2018-09-19 16:03:25 +0700

    add more table generate test case

--- src/question2/MultiplicationTable.Test/TableGeneratorTest.cs
+++ src/question2/MultiplicationTable.Test/TableGeneratorTest.cs
@@ -6,6 +6,8 @@ namespace MultiplicationTable.Test
     public class TableGeneratorTest
     {
         [Theory]
+        [InlineData(0, new string[]{"*", "\n"})]
+        [InlineData(1, new string[]{"*", "1", "\n", "1", "1", "\n"})]
         [InlineData(3, new string[]{"*", "1", "2", "3", "\n", "1", "1", "2", "3", "\n", "2", "2", "4", "6", "\n", "3", "3", "6", "9", "\n"})]
         public void Test_Table_Generate(int inputNumber, string[] expected)
         {
