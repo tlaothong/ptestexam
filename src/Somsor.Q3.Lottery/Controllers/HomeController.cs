6a489b9321f9f33ecf45ab0ab487036421488e1d
Somkiet <somsor@perfenterprise.com>
2018-09-19 16:31:49 +0700

    implement q3

--- src/Somsor.Q3.Lottery/Controllers/HomeController.cs
+++ src/Somsor.Q3.Lottery/Controllers/HomeController.cs
@@ -10,28 +10,59 @@ namespace Somsor.Q3.Lottery.Controllers
 {
     public class HomeController : Controller
     {
+        // public static List<Gambler> Gamblers = new List<Gambler>();
+        static Random ran = new Random();
+        public static List<Gambler> Gamblers = new List<Gambler>();
+
         public IActionResult Index()
         {
-            return View();
+            return View(Gamblers);
         }
 
-        public IActionResult About()
+        public IActionResult Add(string name, int number)
         {
-            ViewData["Message"] = "Your application description page.";
+            if(number < 0 || number > 99)
+            {
+                TempData["ErrorMessage"] = "Number must in range 0-99.";
+                return RedirectToAction(nameof(Index));
+            }
+            if(Gamblers.Any(x => x.Name == name.Trim()))
+            {
+                TempData["ErrorMessage"] = "Duplicate Gambler name.";
+                return RedirectToAction(nameof(Index));
+            }
 
-            return View();
+            Gamblers.Add(new Gambler{
+                Name = name,
+                Number = number,
+            });
+            TempData["Message"] = "Gambler added.";
+
+            return RedirectToAction(nameof(Index));
         }
-
-        public IActionResult Contact()
+        
+        public IActionResult Result()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            var ran = new Random();
+            var ranNum = ran.Next(0, 100);
+            ViewBag.ranNum = ranNum;
+            var winGamblers = Gamblers.Where(x => x.Number == ranNum);
+            return View(winGamblers);
         }
-
-        public IActionResult Privacy()
+        
+        public IActionResult Reset()
         {
-            return View();
+            Gamblers = new List<Gambler>();
+            return RedirectToAction(nameof(Index));
+        }
+        
+        public IActionResult GenerateRandomGambler(int count)
+        {
+            Gamblers.AddRange(Enumerable.Range(1, count).Select(x=>new Gambler{
+                Name = $"Gambler {x}",
+                Number = ran.Next(0, 100),
+            }).ToList());
+            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ src/Somsor.Q3.Lottery/Models/Gambler.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Somsor.Q3.Lottery.Models
+{
+    public class Gambler
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
