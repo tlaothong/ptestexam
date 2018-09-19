9b093954bf257658e1e7a7f14d5aa2d00f9230d0
Earn <earndarkness@perfenterprise.com>
2018-09-19 14:36:12 +0700

    implement code

--- second-question/src/myLibrary/MultiplicationTable.cs
+++ second-question/src/myLibrary/MultiplicationTable.cs
@@ -4,6 +4,23 @@ namespace myLibrary
 {
     public class MultiplicationTable
     {
-        public int[][] Calculate(int amount) => throw new NotImplementedException();
+        public string[,] Calculate(int amount)
+        {
+            var totalAmount = amount + 1;
+            var result = new string[totalAmount, totalAmount];
+
+            for (int i = 0; i < totalAmount; i++)
+            {
+                for (int j = 0; j < totalAmount; j++)
+                {
+                    if (i == 0 && j == 0) result[i, j] = "*";
+                    else if (i == 0 && j != 0) result[i, j] = (1 * j).ToString();
+                    else if (i != 0 && j == 0) result[i, j] = (i * 1).ToString();
+                    else result[i, j] = (i * j).ToString();
+                }
+            }
+            return result;
+        }
+
     }
 }
--- second-question/src/myTest/MultiplicationTableTest.cs
+++ second-question/src/myTest/MultiplicationTableTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using myLibrary;
+using FluentAssertions;
 
 namespace myTest
 {
@@ -10,8 +11,11 @@ namespace myTest
         public void MultiplicationTable_Successed()
         {
             var cal = new MultiplicationTable();
-            var result =  cal.Calculate(0);
-            
+            var result = cal.Calculate(3);
+
+            var expected = new string[,] { { "*", "1", "2", "3" }, { "1", "1", "2", "3" }, { "2", "2", "4", "6" }, { "3", "3", "6", "9" } };
+            result.Should().BeEquivalentTo(expected);
+
         }
     }
 }
