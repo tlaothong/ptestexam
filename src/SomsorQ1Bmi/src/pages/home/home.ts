1894cc76baefe2c1e4cea3c47f55d6092403d280
Somkiet <somsor@perfenterprise.com>
2018-09-19 14:23:52 +0700

    implement q1

--- src/SomsorQ1Bmi/src/pages/home/home.ts
+++ src/SomsorQ1Bmi/src/pages/home/home.ts
@@ -7,8 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  Weight: number;
+  Height: number;
+
+  HeightP2: number;
+  BMI: number;
+  Result: string;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  Calculate() {
+    this.HeightP2 = this.Height * this.Height;
+    this.BMI = (this.Weight / this.HeightP2) * 703;
+
+    if (this.BMI < 18.5) this.Result = "You're underweight. You should consult your doctor!";
+    else if (this.BMI > 25) this.Result = "You're overweight. You should consult your doctor!";
+    else this.Result = "You're within the normal weight range.";
+  }
 }
