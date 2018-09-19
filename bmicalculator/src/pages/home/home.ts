56a8a625c5ee63b408e1bb654f68b948b292e3cf
Aphaphon <pui.aphaphon@gmail.com>
2018-09-19 14:01:06 +0700

    ionic bmicalculator
    Signed-off-by: Aphaphon <pui.aphaphon@gmail.com>

--- bmicalculator/src/pages/home/home.ts
+++ bmicalculator/src/pages/home/home.ts
@@ -7,15 +7,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  weight;
-  hight;
-  result;
+  weight:number;
+  hight:number;
+  result:number;
+  bmi1:string;
+  bmi2:string;
   constructor(public navCtrl: NavController) {
 
   }
-  calculate()
-  {
-           
+  Calculate()
+  {  
+    
+      this.result =  this.weight /(this.hight*this.hight)*703
+      if(this.result >=18.5)
+            {
+              this.bmi1 = "You're within the normal weight range."
+              return this.result + this.bmi1;
+            }
+       if(this.result<18.5)
+            {
+              this.bmi1 = "You're underweight. You should consult your doctor!"
+               return this.result+this.bmi2;
+                
+            }
   }
 
 }
