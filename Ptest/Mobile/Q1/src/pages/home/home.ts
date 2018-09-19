14a343dd41b117e846ac0b0face991ce93629016
thaninpong <thaninpong@perfenterprise.com>
2018-09-19 22:14:58 +0700

    update q1
    Signed-off-by: thaninpong <thaninpong@perfenterprise.com>

--- Ptest/Mobile/Q1/src/pages/home/home.ts
+++ Ptest/Mobile/Q1/src/pages/home/home.ts
@@ -11,15 +11,13 @@ export class HomePage {
   data: string;
   bmi: any = {};
   ibs: any = {};
-  xxx: number;
+  xxx: any={};
 
   constructor(public navCtrl: NavController) {
 
   }
 
   result() {
-    // var bmi = (weight / ((height / 100) * (height / 100)));
-    // var bmi = ((weight * 2.20462262) / (ibs * ibs)) * 703;
     this.ibs = this.height * 0.393;
     this.xxx = ((this.weight*2.2)*703) / ((this.ibs) * (this.ibs))
     this.bmi = this.xxx.toFixed(2);
@@ -35,7 +33,7 @@ export class HomePage {
     {
       this.data = "Congratulations! You are within a healthy weight range.";
     }
-    console.log(JSON.stringify( this.bmi));
+    console.log(JSON.stringify(this.bmi));
   }
 
 }
