f4e9a436e2acb4b13d9c5ce7f4985f7493382e43
Aphaphon <pui.aphaphon@gmail.com>
2018-09-19 15:14:49 +0700

    update api product
    Signed-off-by: Aphaphon <pui.aphaphon@gmail.com>

--- CatalogPos/Controllers/ProductController.1.cs
+++ CatalogPos/Controllers/ProductController.1.cs
@@ -13,10 +13,16 @@ namespace CatalogPos.Controllers
     {    
         private static List<Products> products=new List<Products>  
         {
-            new Products {name="commedy",id="1",price="250"},
-            new Products {name="herror",id="2",price="150"},
-            new Products {name="romance",id="3",price="299"}
-        };      
+            new Products {name="commedy",id="1",price=250,item=10},
+            new Products {name="herror",id="2",price=150,item=10},
+            new Products {name="romance",id="3",price=299,item=10}
+        }; 
+        [HttpGet("{name}/{item}/{price}")]
+        public string Get(string name,int item,int price)
+        {
+            var result = item*price;
+            return "totalamount : " + result  + "baht";
+        }     
         [HttpGet]
         public List<Products> GetAllproduct()
         {
--- CatalogPos/Models/Products.cs
+++ CatalogPos/Models/Products.cs
@@ -4,6 +4,7 @@ namespace CatalogPos.Models
     {
         public string name { get; set; }
         public string id { get; set; }
-        public string price { get; set; }
+        public int price { get; set; }
+        public int item { get; set; }
     }
 }
