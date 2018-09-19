b418ceb5e680c5ee6071e64fcc93d1fe03db100f
kritsada <jokerstudio@hotmail.com>
2018-09-19 15:06:06 +0700

    use bmi calculate

--- src/question1/Calculator/Program.cs
+++ src/question1/Calculator/Program.cs
@@ -6,7 +6,10 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bmiCalculator = new BMICalculator();
+            var result = bmiCalculator.Calculate(50, 40);
+            Console.WriteLine($"Your BMI: {result.BMI}");
+            Console.WriteLine(result.SuggestMessage);
         }
     }
 }
