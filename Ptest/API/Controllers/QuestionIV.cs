42e38ceba3050bec522a4196e87dbff5a964aa05
thaninpong <thaninpong@perfenterprise.com>
2018-09-19 22:00:52 +0700

    update q4
    Signed-off-by: thaninpong <thaninpong@perfenterprise.com>

--- Ptest/API/Controllers/QuestionIV.cs
+++ Ptest/API/Controllers/QuestionIV.cs
@@ -3,31 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using WebApi.Model;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class QuestionIV : Controller
     {
+        MongoClient db;
+        IMongoCollection<Insertitem> Collection;
+
+        public void QuestionIVController()
+        {            
+            db = new MongoDB.Driver.MongoClient("mongodb://admin:admin1234@ds161092.mlab.com:61092/ptest");
+            var test = db.GetDatabase("ptest");
+            Collection = test.GetCollection<Insertitem>("insertitem");
+            //ItemCollection = test.GetCollection<Insertitem>("insertitem");
+        }
+
+        // GET api/values
+        // [HttpGet]
+        // public IEnumerable<string> Get()
+        // {
+        //     return new string[] { "value1", "value2" };
+        // }
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Insertitem> GetItem()
         {
-            return new string[] { "value1", "value2" };
+            var data = Collection.Find(x => true).ToList();
+            return data;
         }
 
         [HttpPost]
-        public void Post([FromBody]string value)
+        public bool Post([FromBody]Insertitem model)
         {
+            model.Id = Guid.NewGuid().ToString();
+
+            Collection.InsertOne(model);
+            return true;
 
         }
-        
-        
-    }
-     public class Additem
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public int price { get; set; }
+
+
     }
 }
