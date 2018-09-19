fa1727ab898b374744e4abded2c5dda200821cc0
kritsada <jokerstudio@hotmail.com>
2018-09-19 14:53:54 +0700

    add more bmi calculate test case

--- src/question1/Calculator.Test/BMICalculatorTest.cs
+++ src/question1/Calculator.Test/BMICalculatorTest.cs
@@ -7,6 +7,10 @@ namespace Calculator.Test
     {
         [Theory]
         [InlineData(50, 40, 21.96875, "You're within the normal weight range.")]
+        [InlineData(40, 40, 17.575, "You're underweight. You should consult your doctor!")]
+        [InlineData(0, 40, 0, "Can't calculate please try again")]
+        [InlineData(40, 0, 0, "Can't calculate please try again")]
+        [InlineData(0, 0, 0, "Can't calculate please try again")]
         public void Test_Calculate_BMI(double weight, double height, double expectedBMI, string expectedMessage)
         {
             var cal = new BMICalculator();
--- src/question1/Calculator/BMICalculator.cs
+++ src/question1/Calculator/BMICalculator.cs
@@ -7,9 +7,25 @@ namespace Calculator
     {
         public CalculateResult Calculate(double weight, double height)
         {
-            var bmi = (weight / (Math.Pow(height, 2))) * 703;
-            var suggestMessage = "You're within the normal weight range.";
+            const double MinimumBMI = 18.5;
+            const double MaximumBMI = 25;
+            var suggestMessage = "You're underweight. You should consult your doctor!";
+
+            var isValidInput = weight != 0 && height != 0;
+            if (!isValidInput)
+            {
+                return new CalculateResult{
+                    SuggestMessage = "Can't calculate please try again"
+                };
+            }
             
+            var bmi = (weight / (Math.Pow(height, 2))) * 703;
+        
+            if(bmi >= MinimumBMI && bmi <= MaximumBMI)
+            {
+                suggestMessage = "You're within the normal weight range.";
+            }
+
             return new CalculateResult{
                 BMI = bmi,
                 SuggestMessage = suggestMessage
