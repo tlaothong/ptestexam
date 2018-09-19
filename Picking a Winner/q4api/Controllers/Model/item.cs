23d7c5356d62453abcf44d5ecd8bf1ba48adaf2b
powerofwind <powerofwind10@gmail.com>
2018-09-19 18:31:33 +0700

    create api and ionic

---
+++ Picking a Winner/q4api/Controllers/Model/item.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace demoapi.Controllers 
+{
+    public class booklist 
+    {
+     
+        public string name { get; set; }
+        public string serialnumber {get; set;}
+        public int unitprice {get; set;}
+
+    }
+}
