656aa5a82a9f68693922d625a8acbb0055d2eb94
kritsada <jokerstudio@hotmail.com>
2018-09-19 14:27:09 +0700

    fix calculate bmi test case

--- src/question1/Calculator.Test/BMICalculatorTest.cs
+++ src/question1/Calculator.Test/BMICalculatorTest.cs
@@ -7,7 +7,7 @@ namespace Calculator.Test
     {
         [Theory]
         [InlineData(50, 40, 21.96875, "You're within the normal weight range.")]
-        public void Test_Calculate_BMI(double weight, double height, string expectedBMI, string expectedMessage)
+        public void Test_Calculate_BMI(double weight, double height, double expectedBMI, string expectedMessage)
         {
             throw new NotImplementedException();
         }
