5035c05943565c52890cd6cbef1da0846ee6e74d
Earn <earndarkness@perfenterprise.com>
2018-09-19 14:55:43 +0700

    Add more test case

--- second-question/src/myLibrary/MultiplicationTable.cs
+++ second-question/src/myLibrary/MultiplicationTable.cs
@@ -4,20 +4,42 @@ namespace myLibrary
 {
     public class MultiplicationTable
     {
-        public string[,] Calculate(int amount)
+        // public string[,] Calculate(int amount)
+        // {
+        //     var totalAmount = amount + 1;
+        //     var result = new string[totalAmount, totalAmount];
+
+        //     for (int i = 0; i < totalAmount; i++)
+        //     {
+        //         for (int j = 0; j < totalAmount; j++)
+        //         {
+        //             if (i == 0 && j == 0) result[i, j] = "*";
+        //             else if (i == 0 && j != 0) result[i, j] = (1 * j).ToString();
+        //             else if (i != 0 && j == 0) result[i, j] = (i * 1).ToString();
+        //             else result[i, j] = (i * j).ToString();
+        //         }
+        //     }
+        //     return result;
+        // }
+
+        public string[] Calculate(int amount)
         {
+            const int minimumAmount = 1;
+            if(amount < minimumAmount) return null;
+
             var totalAmount = amount + 1;
-            var result = new string[totalAmount, totalAmount];
+            var result = new string[totalAmount];
 
             for (int i = 0; i < totalAmount; i++)
             {
                 for (int j = 0; j < totalAmount; j++)
                 {
-                    if (i == 0 && j == 0) result[i, j] = "*";
-                    else if (i == 0 && j != 0) result[i, j] = (1 * j).ToString();
-                    else if (i != 0 && j == 0) result[i, j] = (i * 1).ToString();
-                    else result[i, j] = (i * j).ToString();
+                    if (i == 0 && j == 0) result[i] += "*";
+                    else if (i == 0 && j != 0) result[i] += $" {(1 * j)}";
+                    else if (i != 0 && j == 0) result[i] += $" {(i * 1)}";
+                    else result[i] += $" {(i * j)}";
                 }
+                result[i] = result[i].Trim();
             }
             return result;
         }
--- second-question/src/myTest/MultiplicationTableTest.cs
+++ second-question/src/myTest/MultiplicationTableTest.cs
@@ -7,15 +7,26 @@ namespace myTest
 {
     public class MultiplicationTableTest
     {
-        [Fact]
-        public void MultiplicationTable_Successed()
+        [Theory]
+        [InlineData(1, new string[] { "* 1", "1 1" })]
+        [InlineData(2, new string[] { "* 1 2", "1 1 2", "2 2 4" })]
+        [InlineData(3, new string[] { "* 1 2 3", "1 1 2 3", "2 2 4 6", "3 3 6 9" })]
+        [InlineData(4, new string[] { "* 1 2 3 4", "1 1 2 3 4", "2 2 4 6 8", "3 3 6 9 12", "4 4 8 12 16" })]
+        public void MultiplicationTable_Successed(int amount, string[] expected)
         {
             var cal = new MultiplicationTable();
-            var result = cal.Calculate(3);
-
-            var expected = new string[,] { { "*", "1", "2", "3" }, { "1", "1", "2", "3" }, { "2", "2", "4", "6" }, { "3", "3", "6", "9" } };
+            var result = cal.Calculate(amount);
             result.Should().BeEquivalentTo(expected);
+        }
 
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void MultiplicationTable_Failed(int amount)
+        {
+            var cal = new MultiplicationTable();
+            var result = cal.Calculate(amount);
+            Assert.Null(result);
         }
     }
 }
