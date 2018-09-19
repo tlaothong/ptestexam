8b3b579c908e44baf85f7e2d5ef7c9cc6594d33a
Aphaphon <pui.aphaphon@gmail.com>
2018-09-19 12:00:56 +0700

    create mobile app ui bmicalculate

---
+++ bmicalculator/src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  weight;
+  hight;
+  result;
+  constructor(public navCtrl: NavController) {
+
+  }
+  calculate()
+  {
+           
+  }
+
+}
