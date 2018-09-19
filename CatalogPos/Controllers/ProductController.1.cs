456ba2f8f4c5f7296d83e75b103465a6e3c9922c
Aphaphon <pui.aphaphon@gmail.com>
2018-09-19 14:53:54 +0700

    myproduct

--- CatalogPos/Controllers/ProductController.1.cs
+++ CatalogPos/Controllers/ProductController.1.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CatalogPos.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogPos.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ProductController : ControllerBase
-    {       
-        [HttpGet]
-        public string Get()
+    {    
+        private static List<Products> products=new List<Products>  
         {
-            return "";
+            new Products {name="commedy",id="1",price="250"},
+            new Products {name="herror",id="2",price="150"},
+            new Products {name="romance",id="3",price="299"}
+        };      
+        [HttpGet]
+        public List<Products> GetAllproduct()
+        {
+            return products;
+        }
+        [HttpPost]
+        public string CreateProduct([FromBody]Products data)
+        {
+            products.Add(data);
+            return data.name;
         }
     }
 }
