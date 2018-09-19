2a08899310c661f2c5141a682c1a57ae35d53737
Somkiet <somsor@perfenterprise.com>
2018-09-19 15:21:55 +0700

    init q3

---
+++ src/Somsor.Q3.Lottery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Somsor.Q3.Lottery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
