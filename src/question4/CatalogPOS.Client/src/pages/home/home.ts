5c9e017346711ca9b953e3f52e19ad28f8194b77
kritsada <jokerstudio@hotmail.com>
2018-09-19 18:18:51 +0700

    implement search page

--- src/question4/CatalogPOS.Client/src/pages/home/home.ts
+++ src/question4/CatalogPOS.Client/src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { CatalogServiceProvider } from '../../providers/catalog-service/catalog-service';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+  Products: any[] = [];
+  SearchKey: any = {};
+  SearchText: string;
+
+  SearchOptions: { key: string, value: string }[] = [
+    { key: "ชื่อสินค้า", value: "Name" },
+    { key: "รหัสสินค้า", value: "SerialNumber" },
+    { key: "ราคาสินค้า", value: "UnitPrice" },
+  ];
+
+  constructor(public navCtrl: NavController,
+    private catalogService: CatalogServiceProvider,
+    private navParams: NavParams) {
 
   }
 
+  onSubmit(){
+    this.catalogService.Search(this.SearchKey, this.SearchText).then(data => {
+      this.Products = data;
+    });
+  }
+
+  Buy(product: any)
+  {
+
+  }
 }
